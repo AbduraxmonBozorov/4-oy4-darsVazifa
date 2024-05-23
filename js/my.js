d8022d3eb3514a92c2c509fca701d9853e92d24b
AbduraxmonBozorov <abduraxmonbozorov1999@gmail.com>
2024-05-23 18:43:26 +0500

    4-oy  4-dars vazifasi yuklandi

---
+++ js/my.js
@@ -0,0 +1,104 @@
+
+
+// 1-masala
+// let t=9;
+// let T=48;
+// let ketishVaqti=(t+T)%24;
+// console.log(ketishVaqti);
+
+
+// 2-masala
+// let selsius=32;
+// let kelvin=selsius+273.15;
+// let fr=selsius*1.8+32;
+// console.log("Kelvin: "+kelvin);
+// console.log("Fahrenheit: "+fr);
+
+
+
+
+// 3-masala
+// let Ali=x;
+// let Vali=y;
+// x=y+N;
+// y=X/N;
+// Ishlay olmadim!
+
+
+
+
+// 4-masala
+
+// let n=+prompt("Tarvozning og'irligini kiriting ");
+// let a=+prompt("Tarvuzdagi suv miqdorining foizini kiriting");
+// let b=+prompt("b% qiymat");
+// let tKg=n*a/100;
+// let tKg2=tKg*(b/100);
+// console.log(tKg2);
+
+
+
+
+// 5-masala
+// let son=+prompt("Ko'pburchak  tomonlari sonini kiriting");
+// let deg=180*(son-1);
+// console.log(deg);
+
+
+
+// 6-masala
+// let soat=15;
+// let minut=24;
+// let addMinut=56;
+// if(minut+addMinut > 59){
+//     soat++;
+//     minut=(minut+addMinut)%60;
+// }else{
+
+// }
+// console.log(soat + " : "+minut);
+
+
+
+// 7-masala
+// let sardor=+prompt("Sardorning yoshi");
+// let buvi=+prompt("Buvisining yoshi");
+// // sardor+x=(buvi+x)/2
+// // x=B-2*sardor
+// let x=buvi-2*sardor;
+// console.log(x);
+
+
+
+
+
+
+// 8-masala
+// let soni=+prompt("Xodimlar soni");
+// let summ=0;
+// for(let i=0; i<soni-2; i++){
+//     summ+=+prompt(`${i+1} - xodim maoshi`);
+// }
+// let ortacha=summ/(soni-2);
+// console.log(ortacha);
+
+
+
+
+// 9-masala
+// let N=+prompt("Qavariq ko'pburchak tomonlari soni");
+// let a=(N*(N-1)*(N-2)*(N-3))/24;
+// console.log(a);
+
+
+
+
+// 10-masala
+// let T=+prompt("kino vaqti");
+// let A=+prompt("1 sekund hajmi");
+// let B=+prompt("internet tezligi");
+// // B+n=T/A
+// // n=(T/A)-B;
+// let delay=(T/A)-B;
+// delay=Math.ceil(delay)
+// console.log(delay);
